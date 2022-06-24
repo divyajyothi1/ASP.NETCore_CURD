6f096b524ea42a0d3451db92a6fbc7f73ad01d08
Divya <Divya@LAPTOP-DCKK1SPB>
2022-06-24 11:39:05 -0500

    Add project files.

---
+++ ASP.NETCore_CURD/Data/ASPNETCore_CURDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ASP.NETCore_CURD.Models;
+
+namespace ASP.NETCore_CURD.Data
+{
+    public class ASPNETCore_CURDContext : DbContext
+    {
+        public ASPNETCore_CURDContext (DbContextOptions<ASPNETCore_CURDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ASP.NETCore_CURD.Models.Student>? Student { get; set; }
+    }
+}
---
+++ ASP.NETCore_CURD/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ASP.NETCore_CURD.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ASPNETCore_CURDContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ASPNETCore_CURDContext") ?? throw new InvalidOperationException("Connection string 'ASPNETCore_CURDContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
